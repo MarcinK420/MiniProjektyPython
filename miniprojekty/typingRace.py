841e70d51eb97ee102945ba9722f6d72ffcf8d44
MarcinK420 <marcin.kruk@uth.pl>
2024-12-27 11:25:47 +0100

    updated typingRace.py
    added timer

--- miniprojekty/typingRace.py
+++ miniprojekty/typingRace.py
@@ -1,43 +1,57 @@
 import random
+import time
 points = 0
 
-def updatePoints ():
+def update_points():
     global points
     points += 1
     return points
 
-def RandomSentance ():
-    sentance = ""
+def get_current_time():
+    current_time = time.time()
+    return current_time
+
+def update_time(timeA, timeB):
+    global time_difference
+    time_difference = timeB - timeA
+    return time_difference
+
+def random_sentence():
+    sentence = ""
     words = ["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"]
     for i in range(0, random.randint(1, 10)):
         random_word = random.choice(words)
         if i == 0:
-            sentance = random_word
+            sentence = random_word
         else:
-            sentance = sentance +" "+ random_word
-    return sentance
+            sentence = sentence + " " + random_word
+    return sentence
 
-
-def TypingRace ():
-    sentance = RandomSentance()
+def typing_race():
+    sentance = random_sentence()
     print(sentance)
     user_input = input("Type the sentance above: ")
     if user_input.lower() == sentance.lower():
         print("You typed the sentance correctly!")
-        updatePoints()
+        update_points()
     else:
         print("You typed the sentance incorrectly!")
     return points
 
-def play():
+def play_game():
     print("Welcome to the typing game!")
-    print("Type the sentance below as fast as you can!")
+    print("Type the sentence below as fast as you can!")
     start = input("Press Y to start the game!")
-    if start == "Y":
+    if start.lower() == "y":
+        time1 = get_current_time()
         for i in range(0, 5):
-            points = TypingRace()
+            typing_race()
+        time2 = get_current_time()
+        update_time(time1, time2)
 
-play()
+time_difference = 0
+
+play_game()
 print("You got", points, "points!")
-        
+print("You took", time_difference, "seconds to complete the game!")        
 
